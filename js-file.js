14f9ac21edc53138c17c6d08510c0bf0ebc8939a
Shashwot Adhikari <shashwot123@gmail.com>
2025-07-10 18:16:51 +0545

    Fix equals button
    Fix equals function to continue calculation after equals button is pressed

--- js-file.js
+++ js-file.js
@@ -1,3 +1,4 @@
+// Arithmetic functions
 function add(){
     return num1+num2;
 }
@@ -20,6 +21,7 @@ function display(number){
     dis.textContent = number;
 }
 
+// Operate function to call the arithmetic functions based on the operator
 function operate(operator){
     if (operator == '+'){
         num1 = add();
@@ -37,10 +39,10 @@ function operate(operator){
     num2 = null;
 }
 
-let number = '';
-let num1 = null;
-let num2 = null;
-let operator = '';
+let number = ''; // To store input digits
+let num1 = null; // To store first operand
+let num2 = null; // To store second operand
+let operator = ''; // To store the operator (+,-,X,/)
 
 // What to do when number buttons are clicked
 const numberButtons = document.querySelectorAll('.number');
@@ -56,17 +58,25 @@ numberButtons.forEach((button) => {
 const operatorButtons = document.querySelectorAll('.operator');
 operatorButtons.forEach((button) => {
     button.addEventListener('click', () =>{
-        if (num1 !== null){
-            num2 = parseInt(number);
-            if (operator != ''){
-                operate(operator);
+        // If the user has typed a number, convert it into a number
+        if (number !== ''){
+            // If num1 exists, assign the number provided by the user to num2
+            if (num1 !== null){
+                num2 = parseInt(number);
+                // The previous operation is done if it exists
+                if (operator !== ''){
+                    operate(operator);
+                }
+                // Clear input
+                number = '';
+            }else{
+                // If num1 doesnot exist, assign the number to num1
+                num1 = parseInt(number);
+                // Clear input
+                number = '';
             }
-            number = '';
-        }else{
-            num1 = parseInt(number);
-            number = '';
         }
-        
+        // Store the operator based on the button pressed
         operator = button.textContent;
     });
 });
@@ -74,9 +84,15 @@ operatorButtons.forEach((button) => {
 // What to do when equals button is clicked
 const equals = document.querySelector('.equals');
 equals.addEventListener('click', () =>{
-    if (!num2){
+    // If the user has entered a number, store it in num2
+    if (number !== ''){
         num2 = parseInt(number);
-        number = '';
     }
-    operate(operator);
+    // If there is an operator and a second number, do the operation
+    if (operator && num2 !== null){
+        operate(operator);
+        // Clear the input and the operator after operation done
+        number = '';
+        operator = '';
+    }  
 });
